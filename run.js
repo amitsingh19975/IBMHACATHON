fd6d18d1f26d094459d784f56ee052290b17afbf
svr8 <shikhar.vaish90@gmail.com>
2018-09-14 19:34:47 +0530

    Added python script execution

--- run.js
+++ run.js
@@ -1,23 +1,6 @@
 const cmd = require('node-cmd');
 
 module.exports = {
-  foo: function () {
-    // whatever
-    
-    console.log("TEST");
-  },
-  bar: function () {
-    // whatever
-  },
-  test: function() {
-    cmd.get(
-      'python script.py',
-      function(err, data, stderr) {
-        console.log('the current working dir is : ',data);
-      }
-    )
-  },
-
   run: function(command, callback) {
     cmd.get(command, function(err, data, stderr) {
       if(stderr) console.log(stderr);
