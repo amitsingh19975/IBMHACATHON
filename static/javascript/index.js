3b3dd46545d4485f102cb0ef009af052fda28ca2
Amit Singh <amitsingh19975@gmail.com>
2018-09-14 18:52:04 +0530

    updated

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,6 +1,36 @@
-new Vue({
+vm = new Vue({
     el: "#app",
     data: {
         name: "asdf",
-    }
+        map: "",
+        google: "<script src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyBzAaIGJ44drCpI0VYw0L4HOMc6UZIQXkY&callback=initMap\"async defer></script>",
+    },
+    methods: {
+        initMap() {
+            this.map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: -34.397,
+                    lng: 150.644
+                },
+                zoom: 8
+            });
+        },
+        mapElementCreate() {
+            const google = document.querySelector("#google");
+            const script = document.createElement('script');
+            script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDArGV6xMnqpsLRY8rtL4tLN7EfQqzM6UU&callback=vm.initMap";
+            script.async = true;
+            script.defer = true;
+            google.appendChild(script);
+        },
+    },
+    beforeCreate() {
+        this.$nextTick = function () {
+            this.initMap();
+        };
+    },
+    beforeMount() {
+        this.mapElementCreate();
+    },
+
 });
