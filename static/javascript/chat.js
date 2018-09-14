638a6bb17f1932fe4df07219e167100d19fa9e7c
Amit Singh <amitsingh19975@gmail.com>
2018-09-14 23:12:33 +0530

    chat

---
+++ static/javascript/chat.js
@@ -0,0 +1,17 @@
+new Vue({
+    el:"#app",
+    data:{
+        formHide: false,
+    },
+    methods:{
+        login(type = false){
+            if(!type){
+
+            }else{
+
+            }
+
+            this.formHide = true;
+        }
+    }
+});
