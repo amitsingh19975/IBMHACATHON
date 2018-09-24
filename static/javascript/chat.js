2eaa01b82f779d76cc49257aa5c9b131540112ba
Amit Singh <amitsingh19975@gmail.com>
2018-09-25 01:03:43 +0530

    UI

--- static/javascript/chat.js
+++ static/javascript/chat.js
@@ -4,50 +4,15 @@ new Vue({
         formHide: false,
         messages:[],
         currentUser:'amit',
-        message:'',
+        item:'',
     },
     methods:{
-        login(type = false){
-            if(!type){
-
-            }else{
-
-            }
-
-            this.formHide = true;
+        login(){
         },
-        scrollBottom(){
-            const chat = document.querySelector("#chat-system");
-            chat.scrollTop = chat.scrollHeight;
-        },
-        readMessages(){
-            if(this.messages.length > 1000){
-                let per = Math.floor(this.messages.length*80/100);
-                this.messages.splice(0,per);
-            }
-            message = {};
-            users = ['amit','ayush','shikar'];
-            message.user = users[Math.floor(Math.random() * 3)];
-            message.mess = this.message;
-            this.message = '';
-            if(this.currentUser === message.user){
-                message.me = true;
-            }else{
-                message.me = false;
-            }
-
-            message.date = new Date();
-
-            this.messages.push(message);
-            setTimeout(this.scrollBottom,2);
+        readInventory(){
+            
         },
     },watch:{
-        formHide(){
-            this.$nextTick(()=>{
-                if(this.formHide){
-                    this.scrollBottom();
-                }
-            });
-        },
+        
     }
 });
